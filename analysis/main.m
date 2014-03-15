155c9adb2cbc96b96fbda9467e368e2f6a6fa357
James Folberth <jamesfolberth@gmail.com>
2014-03-15 14:54:39 -0600

    moved readme to readme.d; added a picture

--- analysis/main.m
+++ analysis/main.m
@@ -1,3 +1,5 @@
 
+clear;
+
 %plot_orbit('../data/orbit_2014_03_14_2130.h5','herr')
 plot_orbit('../data/orbit_2014_03_14_2130.h5','orb.h')
--- analysis/plot_orbit.m
+++ analysis/plot_orbit.m
@@ -53,8 +53,12 @@ elseif strcmp(plot_type,'orb.h')
    %plot(t, orb.i*180/pi)
    %plot(t, orb.h)
    %plot(t, orb.e)
-   %plot(t,orb.e.*sin(orb.omega+orb.Omega))
+   plot(t,orb.e.*sin(orb.omega+orb.Omega))
+   axis([t(1),t(end)]);
+   title('h=e\sin(\omega+\Omega)','Interpreter','tex');
+   xlabel('t [yr]');
+   ylabel('h []');
 
-
+   print('pluto_h_1Gyr.png','-dpng');
 
 end 
